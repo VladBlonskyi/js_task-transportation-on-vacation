8a4ee3fa0f8f757a8e1aaa8e9905f8fd2835a36c
VladBlonskyi <vladblonskyi@gmail.com>
2025-03-04 15:02:47 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,32 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const oneDayCost = 40;
+  let totalCost = oneDayCost * days;
+
+  if (days < 3) {
+    return totalCost;
+  }
+
+  if (days < 7) {
+    return (totalCost -= 20);
+  }
+
+  return (totalCost -= 50);
 }
 
 module.exports = calculateRentalCost;
+
+/*
+
+Кожен день 40$
+7 або більше днів 50$ знижки на сумму
+на 3 або більше днів, ви отримаєте знижку $20 на загальну суму.
+
+Examples:
+
+calculateRentalCost(1); // 40
+calculateRentalCost(3); // 100
+calculateRentalCost(7); // 230
+
+*/
